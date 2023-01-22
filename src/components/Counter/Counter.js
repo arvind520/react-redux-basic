42d54cfae42d6ae0102349498a2f8e4da2cedf60
Arvind Chauhan <40683239+arvind520@users.noreply.github.com>
2023-01-22 14:56:07 +0530

    features/addRedux added redux using class component

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react'
+import { decrement, increment } from '../../Store/actions/counterActions';
 import { store } from '../../Store/store'
 
 export default class Counter extends Component {
     constructor(props){
         super(props);
+        this.onIncrement = this.onIncrement.bind(this);
+        this.onDecrement = this.onDecrement.bind(this);
+        this.updateState = this.updateState.bind(this);
         this.state = { counter: 0 }
     }
     componentDidMount(){
-        const state = store.getState();
-        console.log(state)
+        store.subscribe(() => this.updateState())
     }
+
+    updateState(){
+        const state = store.getState();
+        this.setState({
+            counter: state.counter,
+        })
+    }
+
+    onIncrement(){
+        store.dispatch(increment());
+    }
+    onDecrement(){
+        store.dispatch(decrement());
+    }
+
     render() {
         const {counter} = this.state;
         return (
         <div>
             <div><strong>Counter: </strong> <span>{counter}</span></div> 
-            <button>Increment</button> <button>Decrement</button>
+            <button onClick={this.onIncrement}>Increment</button> <button onClick={this.onDecrement}>Decrement</button>
         </div>
         )
     }
